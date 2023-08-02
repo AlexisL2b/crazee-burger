c5ee4d36f74cda465ac0e4a5dc06f87d6fce5548
AlexisL2b <101928945+AlexisL2b@users.noreply.github.com>
2023-08-02 15:04:53 +0200

    🎨  F01: REFACTO> Restructuring repository/files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,6 @@
-import { useRef } from "react"
-import { useState } from "react"
 import LoginPages from "./components/pages/LoginPages"
 
 function App() {
-  //state
-  const [firstName, setFirstName] = useState("")
-
-  //comportement
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    alert("Bonjour " + firstName)
-    setFirstName("")
-  }
-
-  const handlePrenom = (e) => {
-    setFirstName(e.currentTarget.value)
-  }
-
   //Affichage
   return (
     <div>

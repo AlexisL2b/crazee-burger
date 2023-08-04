6a3c92d7b29f279d3533b5f0cbb7188dfd5d46c4
AlexisL2b <101928945+AlexisL2b@users.noreply.github.com>
2023-08-04 16:01:58 +0200

    📦 Add Package

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import { BsWalletFill } from "react-icons/bs"
+import styled from 'styled-components'
 
 export default function LoginForm() {
   //state
@@ -30,3 +32,25 @@ export default function LoginForm() {
     </form>
   )
 }
+const LoginFormStyled = styled.
+
+
+// 4 méthodes pour ajouter du style a un component 
+
+
+/*
+1. Inline style
+Ajout rapide de css +/- Pas très propre
+
+Exemple:
+<h2 style = {1st acolade pour interpolation js{2th pour l'objet js pour ecrire du css in js attentdue par ma propriétée style \\key : "value"//}}>Connectez-vous</h2>
+
+2. Object   style
+
+3. Modules CSS (avec class name)
+
+4. global style(inedx.css)
+
+5. Style component
+
+*/

d007ac0075f4b7f6c460af970732b079f9161f6e
AlexisL2b <101928945+AlexisL2b@users.noreply.github.com>
2023-08-04 10:12:47 +0200

    📦 F02: Add react dom rooter v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react"
 import LoginPages from "./components/pages/login/LoginPages"
 import OrderPages from "./components/pages/order/OrderPages"
 
 function App() {
+  //state
+  const [url, setUrl] = useState("/")
+
+  //comportement
+
   //Affichage
   return (
     <div>
       <LoginPages />
-      <OrderPages />
     </div>
   )
 }
--- src/components/pages/order/OrderPages.jsx
+++ src/components/pages/order/OrderPages.jsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 export default function OrderPages() {
   return <div>OrderPages</div>
 }

e6f3097a2e7d7ef88a5882ff0e669aa6be6b70db
AlexisL2b <101928945+AlexisL2b@users.noreply.github.com>
2023-08-10 14:59:00 +0200

    🚧 F05:ORDER>CARD> finish Layout of card component

--- src/components/pages/order/main/cards/Card.jsx
+++ src/components/pages/order/main/cards/Card.jsx
@@ -22,26 +22,49 @@ const CardStyled = styled.div`
   width: 240px;
   padding: 50px 20px 10px 20px;
   flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-start;
+  justify-content: center;
+  align-items: center;
   gap: 15px;
   .picture {
     background-color: blue;
+    width: 200px;
+    height: 145px;
   }
   .desc {
     background-color: blue;
+    display: flex;
+    padding: 0px 5px 5px 5px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: -7.5px;
   }
   .title {
     background: green;
+    display: flex;
+    height: 46px;
+    flex-direction: column;
+    justify-content: center;
   }
   .infos {
     background: green;
+    display: flex;
+    align-items: center;
 
     .price {
       background: orange;
+
+      display: flex;
+      padding: 22.25px 50px 22.25px 0px;
+      align-items: flex-start;
     }
     .button {
       background: orange;
+      display: flex;
+      width: 95px;
+      height: 38px;
+      padding: 12px 26.3px 12px 26.5px;
+      justify-content: center;
+      align-items: center;
     }
   }
 `

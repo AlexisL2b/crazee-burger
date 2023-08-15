d40511a8b14a9ad13cd3ffc68d6b1d7bd9295810
AlexisL2b <101928945+AlexisL2b@users.noreply.github.com>
2023-08-15 13:39:44 +0200

    💄 F07: ORDER>MAIN>PANNELBUTTON> Add style.

--- src/components/pages/order/main/pannel/Pannel.jsx
+++ src/components/pages/order/main/pannel/Pannel.jsx
@@ -1,30 +1,53 @@
 import React from "react"
 import { styled } from "styled-components"
 import PannelButton from "./PannelButton"
+import { FiChevronDown } from "react-icons/fi"
+import { AiOutlinePlus } from "react-icons/ai"
+import { MdModeEditOutline } from "react-icons/md"
+import { theme } from "../../../../../theme"
 
 export default function Pannel() {
   return (
     <PannelStyled>
       <div className="button-container">
-        <PannelButton />
-        <PannelButton />
-        <PannelButton />
+        <PannelButton icons={<FiChevronDown />} className="chevron_button" />
+        <PannelButton
+          labbel={"Ajouter un produit"}
+          icons={<AiOutlinePlus />}
+          className="other_button"
+        />
+        <PannelButton
+          labbel={"Modifier un produit"}
+          icons={<MdModeEditOutline />}
+          className="other_button"
+        />
       </div>
       <div className="pannel-table">PannelTab</div>
     </PannelStyled>
   )
 }
 const PannelStyled = styled.div`
-  border: 1px solid red;
   position: absolute;
   top: 700px;
   width: 1400px;
+
   .button-container {
-    background: blue;
+    padding: 0px 70px;
+    display: flex;
   }
   .pannel-table {
-    background: pink;
+    box-sizing: content-box;
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    border-bottom-right-radius: ${theme.borderRadius.extraRound};
+    box-shadow: 0px -6px 8px -2px rgba(0, 0, 0, 0.1);
+    border: 1px solid #e4e5e9;
+    background-color: white;
     height: 250px;
     width: 100%;
+  }
+  .other_button {
+    i {
+      margin-right: 10px;
+    }
   }
 `
--- src/components/pages/order/main/pannel/PannelButton.jsx
+++ src/components/pages/order/main/pannel/PannelButton.jsx
@@ -1,5 +1,32 @@
 import React from "react"
+import { styled } from "styled-components"
+import { theme } from "../../../../../theme"
 
-export default function PannelButton() {
-  return <button>PannelButton</button>
+export default function PannelButton({ labbel, icons, className }) {
+  return (
+    <PannelButtonStyled className={className}>
+      <i>{icons}</i>
+      {labbel && labbel}
+    </PannelButtonStyled>
+  )
 }
+
+const PannelButtonStyled = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 22px 10px 22px;
+  border-top: 1px solid #e4e5e9;
+  border-right: 1px solid #e4e5e9;
+  border-left: 1px solid #e4e5e9;
+  border-bottom: 2px solid #e4e5e9;
+  background-color: white;
+  color: ${theme.colors.greyDark};
+  border-radius: ${theme.borderRadius.round} ${theme.borderRadius.round} 0px 0px;
+  box-shadow: 0px -6px 8px -2px rgba(0, 0, 0, 0.1);
+
+  cursor: pointer;
+  &:hover {
+    border-bottom: 2px solid white;
+  }
+`

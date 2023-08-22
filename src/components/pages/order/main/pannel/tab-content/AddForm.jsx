a8642d4ca72e5fb6464ea03eda2caf143890a574
AlexisL2b <101928945+AlexisL2b@users.noreply.github.com>
2023-08-22 13:07:52 +0200

    🎨 F07: TAB-CONTENT>ADDFORM> add classname

--- src/components/pages/order/main/pannel/tab-content/AddForm.jsx
+++ src/components/pages/order/main/pannel/tab-content/AddForm.jsx
@@ -1,18 +1,51 @@
 import React from "react"
 import { styled } from "styled-components"
+import PrimaryButton from "../../../../../reusable-ui/PrimaryButton"
 
 export default function AddForm() {
   return (
     <AddFormStyled className="container_form">
-      <form className="form" action=""></form>
+      <form className="form" action="">
+        <div className="picture">
+          <img src="https://picsum.photos/200" alt="" />
+        </div>
+        <div className="right_side">
+          Right
+          <input type="text" name="" id="" placeholder="Nom" />
+          <input type="text" name="" id="" placeholder="Lien" />
+          <input type="text" name="" id="" placeholder="Prix" />
+          <PrimaryButton />
+        </div>
+      </form>
     </AddFormStyled>
   )
 }
 
 const AddFormStyled = styled.div`
-  background-color: red;
   height: 100%;
+  display: grid;
+  align-items: center;
+  justify-content: flex-start;
+  padding-left: 70px;
+
   .form {
-    background-color: ;
+    display: inline-flex;
+    gap: 20px;
+
+    .pictue {
+      background-color: blue;
+    }
+    .right_side {
+      background-color: blue;
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+      input {
+        background-color: green;
+        padding: 8px 16px 8px 24px;
+        width: 605px;
+        height: 19px;
+      }
+    }
   }
 `

5e19693df1dd2460a637568d9aaeaa3870087376
AlexisL2b <101928945+AlexisL2b@users.noreply.github.com>
2023-08-22 11:29:32 +0200

    💄F07: THEME> Add shadows

--- src/components/pages/order/main/pannel/Tab.jsx
+++ src/components/pages/order/main/pannel/Tab.jsx
@@ -30,7 +30,7 @@ const TabStyled = styled.div`
   height: 250px;
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
-  box-shadow: 0px -6px 8px -2px rgba(0, 0, 0, 0.1);
+  box-shadow: ${theme.shadows.subtle};
   border: 1px solid #e4e5e9;
   background-color: white;
 `

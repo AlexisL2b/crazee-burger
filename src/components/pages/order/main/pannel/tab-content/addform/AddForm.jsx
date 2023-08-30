45dbed8783be24c557fec951e0a0d2c321ce6a86
AlexisL2b <101928945+AlexisL2b@users.noreply.github.com>
2023-08-30 12:09:53 +0200

    ♻️ F08: AddForm> Extract image preview

--- src/components/pages/order/main/pannel/tab-content/addform/AddForm.jsx
+++ src/components/pages/order/main/pannel/tab-content/addform/AddForm.jsx
@@ -1,64 +1,21 @@
 import React, { useContext, useState } from "react"
 import { styled } from "styled-components"
-import ImageWrapper from "../../../../../../reusable-ui/cards/ImageWrapper"
 import OrderContext from "../../../../../../../context/OrderContext"
 import InputFields from "./InputFields"
 import ButtonConfirmation from "./ButtonConfirmation"
-// const EMPTY_PRODUCT = {
-//   title: "",
-//   imageSource: "",
-//   price: 0,
-// }
-const EMPTY_NO_IMAGE = "/assets/no_image.png"
+import ImagePreview from "./ImagePreview"
+
 export default function AddForm() {
   //state
   const { newProduct, handleSubmit, handleChange } = useContext(OrderContext)
-  // const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT)
 
-  // const { setIsVisible, handleAdd } = useContext(OrderContext)
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault()
-  //   const newProductToAdd = {
-  //     id: new Date().getTime(),
-  //     ...newProduct,
-  //   }
-  //   handleAdd(newProductToAdd)
-  //   setIsVisible(true)
-  //   setNewProduct(EMPTY_PRODUCT)
-  //   console.log(products)
-  // }
-  // const handleChange = (e) => {
-  //   const name = e.target.name
-  //   const newValue = e.target.value
-  //   setNewProduct({ ...newProduct, [name]: newValue })
-  // }
   //affichage
 
   return (
-    <AddFormStyled
-      className="container_form"
-      action="submit"
-      onSubmit={handleSubmit}
-    >
-      <div className="image_preview">
-        {newProduct.imageSource ? (
-          <ImageWrapper
-            imageSource={newProduct.imageSource}
-            className={"add_picture"}
-          />
-        ) : (
-          <ImageWrapper imageSource={EMPTY_NO_IMAGE} className={"no_image"} />
-        )}
-      </div>
-
+    <AddFormStyled action="submit" onSubmit={handleSubmit}>
+      <ImagePreview />
       <InputFields
         className={"input_fields"}
-        link={
-          newProduct.imageSource
-            ? newProduct.imageSource
-            : "/assets/coming-soon.png"
-        }
         newProduct={newProduct}
         onChange={handleChange}
       />
@@ -80,17 +37,5 @@ const AddFormStyled = styled.form`
   }
   .button_confirmation {
     grid-area: 4/2/4/2;
-  }
-  .image_preview {
-    width: 215px;
-    height: 120px;
-  }
-  .add_picture {
-    height: 100%;
-    width: 100%;
-  }
-  .no_image {
-    height: 100%;
-    width: 100%;
   }
 `
---
+++ src/components/pages/order/main/pannel/tab-content/addform/ImagePreview.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from "react"
+import { styled } from "styled-components"
+import ImageWrapper from "../../../../../../reusable-ui/cards/ImageWrapper"
+import OrderContext from "../../../../../../../context/OrderContext"
+import { theme } from "../../../../../../../theme"
+const EMPTY_NO_IMAGE = "/assets/no_image.png"
+
+export default function ImagePreview() {
+  const { newProduct } = useContext(OrderContext)
+
+  return (
+    <ImagePreviewStyled>
+      {" "}
+      {newProduct.imageSource ? (
+        <ImageWrapper
+          imageSource={newProduct.imageSource}
+          className={"add_picture"}
+        />
+      ) : (
+        <ImageWrapper imageSource={EMPTY_NO_IMAGE} className={"add_picture"} />
+      )}
+    </ImagePreviewStyled>
+  )
+}
+const ImagePreviewStyled = styled.div`
+  width: 215px;
+  height: 150px;
+
+  .add_picture {
+    height: 100%;
+    width: 100%;
+  }
+`

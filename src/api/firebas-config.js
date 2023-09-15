9eae71a2ec359d1ce25d4fdf8d2ba26595c82e7a
AlexisL2b <101928945+AlexisL2b@users.noreply.github.com>
2023-09-15 12:57:40 +0200

    🚧 F13: BDD> install Firebase package

---
+++ src/api/firebas-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCW7azcMPtf2fqq9qgMIe-uBMz6E_qL7Ns",
+  authDomain: "crazee-burger-53092.firebaseapp.com",
+  projectId: "crazee-burger-53092",
+  storageBucket: "crazee-burger-53092.appspot.com",
+  messagingSenderId: "152523818143",
+  appId: "1:152523818143:web:0cd1a933a24800cd7fceaa",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
